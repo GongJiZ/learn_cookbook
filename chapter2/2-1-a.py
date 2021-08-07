a4bcdc0c1e84c704c2ffd7a209126d3a6df7b81c
zhougongji <2779509684@qq.com>
2021-08-07 22:13:08 +0800

    add 1-20 and change 2-1

--- chapter2/2-1-a.py
+++ chapter2/2-1-a.py
@@ -2,3 +2,7 @@
 import re
 line = 'asdf fjdk; afed, fjek,asdf, foo'
 print(re.split(r'[,;\s]\s*', line))
+
+# 分隔字符串且保留风格符号
+print(re.split(r'(;|,|\s)\s*', line))
+print(re.split(r'([;,\s]\s*)', ))
--- chapter2/2-1.py
+++ chapter2/2-1.py
@@ -1,2 +1,4 @@
 # 分隔字符串
 line = 'asdf fjdk; afed, fjek,asdf, foo'
+
+# 分隔字符串且保留风格符号
